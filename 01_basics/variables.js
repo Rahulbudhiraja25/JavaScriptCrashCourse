8c6ebb3481f7f5b652c320781887902db8a2fd83
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2024-11-28 11:38:09 +0000

    Crea

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+let accountName = "asdf"
+var accountAge = 6
+const accountId = 46321
+accountData ="hsjdbjf"
+let accountScope;
+
+// Use only Let and Const for modern js Development as var can work without scope
+// or you can say variable created with var is of global scope whether you create it inside or outside {} || Functions
+
+console.table([accountName,accountAge,accountId,accountData ,accountScope])
+
+
+// You can use Semicolon in your js code but it is not must 
+// But always remember to write clean and efficient code Use camelCasing in variables to make your code up to standards
